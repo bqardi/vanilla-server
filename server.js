8d8f53e72c889758e3585352588e31bdcd0e868b
Sune Seifert <sse@abtion.com>
2024-09-06 14:56:20 +0200

    Move served files to public folder

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 import http from "http";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const server = http.createServer(handleRequest);
 server.listen(3000, onConnect);
 
 function handleRequest(req, res) {
-  fs.readFile("index.html", (err, data) => {
+  const filePath = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "public",
+    req.url.endsWith("/") ? `${req.url}index.html` : req.url
+  );
+
+  let contentType = "text/html";
+  if (path.extname(filePath) === ".css") contentType = "text/css";
+  if (path.extname(filePath) === ".js") contentType = "text/javascript";
+  if (path.extname(filePath) === ".ico") contentType = "image/x-icon";
+
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Error: Could not read file");
       return;
     }
 
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": contentType });
     res.end(data);
   });
 }
