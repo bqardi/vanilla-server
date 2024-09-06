bb9dfbe147741b70903c1f63a604bf7f80aa1c63
Sune Seifert <sse@abtion.com>
2024-09-06 13:45:12 +0200

    Create basic server functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import http from "http";
+
+const server = http.createServer(handleRequest);
+server.listen(3000, onConnect);
+
+function handleRequest(req, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!");
+}
+
+function onConnect(error) {
+  if (error) return console.error(error);
+  console.log("Server is running on port 3000");
+}
