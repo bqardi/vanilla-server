29d728981342219d596fcd678034be86fc050ab8
Sune Seifert <sse@abtion.com>
2024-09-06 14:27:48 +0200

    Read index.html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import http from "http";
+import fs from "fs";
 
 const server = http.createServer(handleRequest);
 server.listen(3000, onConnect);
 
 function handleRequest(req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!");
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error: Could not read file");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
 }
 
 function onConnect(error) {
